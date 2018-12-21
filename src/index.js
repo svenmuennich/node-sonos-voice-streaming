1ec874654b6a5d385ba8049343ed51a19d5ca6b4
Sven Münnich <sven.muennich@viison.com>
2018-12-21 14:45:14 +0100

    Add event names to `index.js`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const client = require('./client');
 const server = require('./server');
+const eventNames = require('./eventNames');
 
 module.exports = {
     client,
+    eventNames,
     server,
 };
