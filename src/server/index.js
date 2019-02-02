cb0a765d1d7fc51e64eb374a84feb380156bac9e
Thomas Nordquist <thomas.nordquist@viison.com>
2019-02-02 21:59:41 +0100

    Improve resilliance and speed

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ const routes = require('./routes');
 const streamSessions = {};
 
 module.exports = async (options) => {
-    const { app, baseUrl, sonosControl, sonosGroupId } = options;
+    const { app, baseUrl, sonosControl } = options;
     const pathPrefix = options.pathPrefix || '/';
 
     // Serve static files like announcement chime
@@ -30,22 +30,37 @@ module.exports = async (options) => {
     const socketIo = new KoaSocketIO();
     socketIo.attach(app);
 
-    const players = await sonosControl.getPlayers();
-    console.log(players);
+    let allPlayersGroupId = undefined
+    async function tearDownStream(streamId) {
+        const sonosSessionId = streamSessions[streamId];
 
-    // TODO: Dynamically regroup all speakers in the configured household. Load speakers here.
-    const playerId = 'RINCON_7828CA0F18DA01400';
+        // Stop playback
+        await sonosControl.suspendPlaybackSession(sonosSessionId);
+
+        // Disband the group of all devices
+        await sonosControl.setGroupMembers(allPlayersGroupId, [])
+    }
 
     // Initialize the web socket
-    socketIo.on('connection', (connection) => {
+    socketIo.on('connection', async (connection) => {
         const socket = connection.socket;
         console.log(`New socket ${socket.id} connected`);
 
         socket.on(eventNames.audioLiveStream.setUp, async (payload) => {
+            // Update player context
+            const groupData = await sonosControl.getCachedGroupData();
+            if (!groupData || !groupData.players) {
+                throw new Error('No player info avaiable');
+            }
+
+            const playerIds = groupData.players.map(player => player.id)
+            allPlayersGroupId = await sonosControl.createOrFetchGroup(playerIds)
+
             console.log(`Setting up live audio stream ${payload.streamId}...`);
-            const sononsSessionId = await sonosControl.ensurePlaybackSession(sonosGroupId, sha1(payload.streamId));
-            audioStreamService.createStream(payload.streamId);
-            streamSessions[payload.streamId] = sononsSessionId;
+            const sonosSessionId = await sonosControl.ensurePlaybackSession(allPlayersGroupId, sha1(payload.streamId));
+
+            audioStreamService.createStream(payload.streamId, () => tearDownStream(payload.streamId));
+            streamSessions[payload.streamId] = sonosSessionId;
             socket.emit(eventNames.audioLiveStream.ready, {});
         });
 
@@ -67,20 +82,19 @@ module.exports = async (options) => {
                 const sonosSessionId = streamSessions[payload.streamId];
                 const streamUrl = new URL(`${liveStreamBasePath}/${payload.streamId}`, baseUrl);
                 console.log(`New stream available at ${streamUrl.toString()}`);
-                await sonosControl.loadStreamUrl(sonosSessionId, streamUrl.toString());
+                sonosControl.loadStreamUrl(sonosSessionId, streamUrl.toString());
 
                 // Play an announcement chime clip AFTER starting the audio stream to work around the delay caused by
                 // the SONOS speaker (ca. 7 seconds)
                 const announcementChimeUrl = new URL(`${staticFilesBasePath}/announcement_chime.mp3`, baseUrl);
-                sonosControl.playAudioClipOnPlayer(announcementChimeUrl.toString(), playerId);
+
+                setTimeout(() => sonosControl.playAudioClipOnAllPlayers(announcementChimeUrl.toString()), 1500)
             }
         });
 
         socket.on(eventNames.audioLiveStream.tearDown, async (payload) => {
             console.log(`Tearing down stream ${payload.streamId}...`);
             audioStreamService.endStream(payload.streamId);
-            const sonosSessionId = streamSessions[payload.streamId];
-            await sonosControl.suspendPlaybackSession(sonosSessionId);
         });
     });
 };
